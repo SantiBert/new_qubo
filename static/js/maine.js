f27692d36f11d8d4631ebf63e46b570f76fb2c5f
SantiBert <santibertero34@gmail.com>
2021-01-13 02:53:47 -0300

    finalizado de verdad

--- static/js/maine.js
+++ static/js/maine.js
@@ -26,3 +26,13 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+ScrollReveal().reveal('.ramdom', { delay: 500 });
